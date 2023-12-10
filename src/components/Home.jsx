0e38077bff5363de8e95cb7363e737fcac44240e
Raj Kumar Tiwari <tiwarimanikant1997@gmail.com>
2023-12-10 14:10:59 +0530

    second commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Card from "./Card";
 import CardProduct, {CardProduct2} from "./CardProduct";
 import Header from "./Header";
-import { useLocation, useParams } from "react-router-dom";
 
 
 const Home = () =>{
